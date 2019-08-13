3f39c3c61463506eadc0277984db7812e4849465
Neeko Blomgren <nico.blomgren@gmail.com>
2019-08-13 12:28:34 -0700

    Header work, Sass Integration

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//import '../styles/Header.css';
+
+function Header() {
+
+   
+
+  return (
+    <div className="Header">
+        Neeko Blomgren: Web Developer
+    </div>
+  );
+}
+
+export default Header;
